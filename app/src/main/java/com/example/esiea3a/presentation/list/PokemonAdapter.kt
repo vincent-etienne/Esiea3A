abb1dc395b741390bab3dd12ad075bcc80045a76
vetienne <vincent.eti@gmail.com>
2021-03-22 20:23:42 +0100

    Add image for pokemon

--- app/src/main/java/com/example/esiea3a/presentation/list/PokemonAdapter.kt
+++ app/src/main/java/com/example/esiea3a/presentation/list/PokemonAdapter.kt
@@ -3,8 +3,10 @@ package com.example.esiea3a.presentation.list
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.esiea3a.R
 
 
@@ -16,10 +18,12 @@ class PokemonAdapter(private var dataSet: List<Pokemon>, var listener: ((Int) ->
      */
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView
+        val imageView: ImageView
 
         init {
             // Define click listener for the ViewHolder's View.
             textView = view.findViewById(R.id.pokemon_name)
+            imageView = view.findViewById(R.id.pokemon_img)
         }
     }
 
@@ -46,6 +50,12 @@ class PokemonAdapter(private var dataSet: List<Pokemon>, var listener: ((Int) ->
         viewHolder.itemView.setOnClickListener {
             listener?.invoke(position)
         }
+
+        Glide
+            .with(viewHolder.itemView.context)
+            .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${position + 1}.png")
+            .centerCrop()
+            .into(viewHolder.imageView)
     }
 
     // Return the size of your dataset (invoked by the layout manager)
