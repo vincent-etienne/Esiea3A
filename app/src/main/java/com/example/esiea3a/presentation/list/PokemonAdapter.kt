6fb7217fd92839f78baadd5dcfd1a687567f5e26
vetienne <vincent.eti@gmail.com>
2021-03-22 19:27:51 +0100

    Add listener in PokemonAdapter for onClick

--- app/src/main/java/com/example/esiea3a/presentation/list/PokemonAdapter.kt
+++ app/src/main/java/com/example/esiea3a/presentation/list/PokemonAdapter.kt
@@ -8,7 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.esiea3a.R
 
 
-class PokemonAdapter(private var dataSet: List<Pokemon>) : RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
+class PokemonAdapter(private var dataSet: List<Pokemon>, var listener: ((Pokemon) -> Unit)? = null) : RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
 
     /**
      * Provide a reference to the type of views that you are using
@@ -43,6 +43,9 @@ class PokemonAdapter(private var dataSet: List<Pokemon>) : RecyclerView.Adapter<
         // contents of the view with that element
         val pokemon = dataSet[position]
         viewHolder.textView.text = pokemon.name
+        viewHolder.itemView.setOnClickListener {
+            listener?.invoke(pokemon)
+        }
     }
 
     // Return the size of your dataset (invoked by the layout manager)
--- app/src/main/java/com/example/esiea3a/presentation/list/PokemonListFragment.kt
+++ app/src/main/java/com/example/esiea3a/presentation/list/PokemonListFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.esiea3a.R
@@ -24,7 +25,8 @@ class PokemonListFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
 
-    private val adapter = PokemonAdapter(listOf())
+    private val adapter = PokemonAdapter(listOf(), ::onClickedPokemon)
+
     private val layoutManager = LinearLayoutManager(context)
 
     override fun onCreateView(
@@ -65,5 +67,9 @@ class PokemonListFragment : Fragment() {
                 }
             }
         })
+    }
+
+    private fun onClickedPokemon(pokemon: Pokemon) {
+        findNavController().navigate(R.id.navigateToPokemonDetailFragment)
     }
 }
